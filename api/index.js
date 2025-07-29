dc89668e1bd0f0ae78bfc24ca7173b4687af3e2b
meo2326cc <cccmsp1@gmail.com>
2025-07-29 16:08:36 +0800

    create api folder

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default function handler(req, res) {
   const { name = 'World' } = req.query
   return res.json({
     message: `Hello ${name}!`,
